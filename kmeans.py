534c4a4d3999ff02013fe1639062089ebe6e4525
Ardo Dlamini <ardodlamini@gmail.com>
2021-05-20 19:15:10 +0200

    Initial Centroids set.

--- kmeans.py
+++ kmeans.py
@@ -9,9 +9,22 @@ def main():
 	for line in my_file:
 		if(line.find("#")==-1):
 			x, y = (int(s) for s in line.split())
-			arr.append([id, x, y, 0]) #last element is the associated cluster, 0 means none.
+			arr.append([id, x, y, 0, 4]) #element 3 is the associated cluster, 0 means none. element 4 is the previous cluster the point was in.
 			id = id + 1
-		
+			
+	centroids=[[]]*3 #array of cluster centres. 
+	
+	#Set initial centroids.
+	for x in arr:
+		if(x[0]==0):
+			centroids[0]= [x[1],x[2]]
+		elif(x[0]==3):
+			centroids[1]= [x[1],x[2]]
+		elif(x[0]==6):
+			centroids[2]= [x[1],x[2]]
+	
+	print(centroids)
+	
 	my_file.close()
 	print(arr)
 
